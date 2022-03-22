27d542ac894da140b994226c2b2252224c425353
Jefferson Santos <jeffersonscr12@gmail.com>
2022-03-21 22:14:41 -0300

    categoriesRepository

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,29 @@
+import { Category } from "../model/category";
+
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+export class CategoriesRepository {
+  private categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ name, description }: ICreateCategoryDTO) {
+    const category = new Category();
+    Object.assign(category, {
+      name,
+      description,
+      createdAt: new Date(),
+    });
+
+    this.categories.push(category);
+  }
+
+  list() {
+    return this.categories;
+  }
+}
