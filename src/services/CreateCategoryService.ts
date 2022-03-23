919c798bf468042e3f552385995142a0373f22a9
Jefferson Santos <jeffersonscr12@gmail.com>
2022-03-22 21:50:18 -0300

    categoryService

---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,20 @@
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+export class CreateCategoryService {
+  constructor(private categoriesRepository: CategoriesRepository) {}
+
+  execute({ name, description }: IRequest): void {
+    const categoryAlreadExists = this.categoriesRepository.findByName(name);
+
+    if (categoryAlreadExists) {
+      throw new Error("Category alread exists!");
+    }
+
+    this.categoriesRepository.create({ name, description });
+  }
+}
